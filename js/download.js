f385deff375525220221d791cef250b0b85983f3
Mohit Kumbhare <emkay@Mohits-MacBook-Air.local>
2023-03-14 16:51:54 +0530

    Download from GDrive working

--- js/download.js
+++ js/download.js
@@ -1,46 +1,72 @@
 const { google } = require('googleapis');
-const { OAuth2Client } = require('google-auth-library');
-const credentials = require('./credentials.json');
+const fs = require('fs');
+const readline = require('readline');
+const { promisify } = require('util');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const CLIENT_ID = 'id';
-const CLIENT_SECRET = 'secret';
-const REDIRECT_URI = 'http://localhost:3000/oauth2callback';
 const SCOPES = ['https://www.googleapis.com/auth/drive.readonly'];
+const TOKEN_PATH = 'token.json';
 
-async function authorize() {
-  const oAuth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+// Load client secrets from a local file.
+fs.readFile('credentials.json', async (err, content) => {
+  if (err) return console.error('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Drive API.
+  try {
+    const credentials = JSON.parse(content);
+    const { client_secret, client_id, redirect_uris } = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+      client_id,
+      client_secret,
+      redirect_uris[0]
+    );
+    // Check if we have previously stored a token.
+    let token = null;
+    try {
+      token = await promisify(fs.readFile)(TOKEN_PATH);
+      oAuth2Client.setCredentials(JSON.parse(token));
+    } catch (err) {
+      token = await getAccessToken(oAuth2Client);
+    }
+    const drive = google.drive({ version: 'v3', auth: oAuth2Client });
+    const fileId = '1YSb8VDThRIMGwkpobWJfIb3q24wjZaIH';
+    const destPath = './new.pdf';
+    const dest = fs.createWriteStream(destPath);
+    const res = await drive.files.get(
+      { fileId, alt: 'media' },
+      { responseType: 'stream' }
+    );
+    res.data
+      .on('end', () => console.log('File download complete.'))
+      .on('error', err => console.error('Error downloading file:', err))
+      .pipe(dest);
+  } catch (err) {
+    console.error('Error:', err);
+  }
+});
 
-  // If the user has previously authorized your application, you can use the refresh token
-  // to obtain a new access token without prompting the user to authenticate again.
-  if (credentials.refresh_token) {
-    oAuth2Client.setCredentials(credentials);
-  } else {
-    // If this is the first time the user is authorizing your application, generate a URL
-    // for the user to grant permission to your application to access their Google Drive.
+function getAccessToken(oAuth2Client) {
+  return new Promise((resolve, reject) => {
     const authUrl = oAuth2Client.generateAuthUrl({
       access_type: 'offline',
       scope: SCOPES,
     });
     console.log('Authorize this app by visiting this url:', authUrl);
-
-    // Once the user has granted permission to your application, they will be redirected
-    // back to the redirect URI specified in the Google Cloud Console. Extract the code
-    // from the query string and exchange it for an access token and refresh token.
-    const code = '4/0AWtgzh6O8sXDzrr-0jZSKrvgYCIk4Z8hfCUZxmnDsvcoZZjCZbFUlT6PHqZEDjRiBGwGTQ'; // replace with the actual code obtained from the redirect URL
-    const { tokens } = await oAuth2Client.getToken(code);
-    oAuth2Client.setCredentials(tokens);
-    console.log('Access token:', tokens.access_token);
-    console.log('Refresh token:', tokens.refresh_token);
-  }
-
-  return oAuth2Client;
+    rl.question('Enter the code from that page here: ', async code => {
+      rl.close();
+      try {
+        const { tokens } = await oAuth2Client.getToken(code);
+        oAuth2Client.setCredentials(tokens);
+        // Store the token to disk for later program executions
+        await promisify(fs.writeFile)(TOKEN_PATH, JSON.stringify(tokens));
+        console.log('Token stored to', TOKEN_PATH);
+        resolve(JSON.stringify(tokens));
+      } catch (err) {
+        console.error('Error retrieving access token:', err);
+        reject(err);
+      }
+    });
+  });
 }
-
-async function getAuth() {
-  const auth = await authorize();
-  const drive = google.drive({ version: 'v3', auth });
-  const response = await drive.files.list();
-  console.log(response.data);
-}
-
-getAuth();
