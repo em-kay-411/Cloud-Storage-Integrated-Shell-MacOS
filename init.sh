9661a0d1d55fea8ee0884a9c29e1f82a4ea714e4
Mohit Kumbhare <emkay@Mohits-MacBook-Air.local>
2023-04-14 15:07:58 +0530

    init updat

--- init.sh
+++ init.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if command -v pm2 >/dev/null 2>&1 ; then
+  echo "pm2 is already installed"
+else
+  echo "pm2 is not installed, installing now..."
+  npm install pm2 -g && pm2 update
+fi
+
 echo "** Installing Node Packages **"
 npm install
 echo "** Setting permissions **"
